1307ca0aee87a0ae5933ffbc4c9dfd995c989661
ksaotome <ksaotome@student.42tokyo.jp>
2026-05-24 21:05:42 +0900

    aa

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,31 @@
 /*   By: ksaotome <ksaotome@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 21:20:17 by ksaotome          #+#    #+#             */
-/*   Updated: 2026/05/18 21:20:42 by ksaotome         ###   ########.fr       */
+/*   Updated: 2026/05/24 20:54:11 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s && s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (s && *s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (s && (char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}

d1c5ec462a93fbee3124f028135a5ef09002fbb0
ksaotome_mac <saopan834@gmail.com>
2026-05-26 02:09:37 +0900

    aaa

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,97 @@
 /*   By: ksaotome <ksaotome@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 21:20:49 by ksaotome          #+#    #+#             */
-/*   Updated: 2026/05/24 19:23:30 by ksaotome         ###   ########.fr       */
+/*   Updated: 2026/05/26 02:09:20 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*append_str(char *stash, char *buffer)
+{
+	char	*joined;
+
+	joined = ft_strjoin(stash, buffer);
+	free(stash);
+	return (joined);
+}
+
+static char	*fill_stash(int fd, char *stash)
+{
+	char	*buf;
+	ssize_t	bytes_read;
+
+	buf = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buf)
+	{
+		free(stash);
+		return (NULL);
+	}
+	bytes_read = 1;
+	while (!ft_strchr(stash, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buf);
+			free(stash);
+			return (NULL);
+		}
+		buf[bytes_read] = '\0';
+		stash = append_str(stash, buf);
+	}
+	free(buf);
+	return (stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	size_t	i;
+
+	if (!stash || !stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	return (ft_substr(stash, 0, i));
+}
+
+static char	*update_stash(char *stash)
+{
+	char	*new_stash;
+	size_t	i;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	i++;
+	new_stash = ft_substr(stash, i, ft_strlen(stash) - i);
+	free(stash);
+	return (new_stash);
+}
+
 char	*get_next_line(int fd)
 {
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	stash = fill_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = update_stash(stash);
+	return (line);
 }
